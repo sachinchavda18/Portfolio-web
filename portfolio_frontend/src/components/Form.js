c8b139369b85300ecba5d0009e7be2b21d9378e8
sachinchavda18 <120299726+sachinchavda18@users.noreply.github.com>
2023-11-24 17:49:54 +0530

    contact form email varification added

--- portfolio_frontend/src/components/Form.js
+++ portfolio_frontend/src/components/Form.js
@@ -3,12 +3,16 @@ import "../css/FormStyle.css";
 import React from "react";
 
 const Form = () => {
+  const handleSubmit = () => {
+    alert("Thank you! Your details have been successfully submitted.");
+  };
+  
   return (
-    <div className="form">
+    <form className="form" action="https://formspree.io/f/meqbbqwe" method="post" onSubmit={handleSubmit}>
       <label htmlFor="name">Your Name</label>
-      <input type="text" name="name" placeholder="Enter Your name" />
+      <input type="text" name="name" placeholder="Enter Your name" required/>
       <label htmlFor="email">Email</label>
-      <input type="email" name="email" placeholder="abc@abc.com"/>
+      <input type="email" name="email" placeholder="abc@abc.com" required/>
       <label htmlFor="msg">Message</label>
       <textarea
         name="msg"
@@ -17,56 +21,8 @@ const Form = () => {
         placeholder="Type Your Message here."
       ></textarea>
       <button className="btn">Submit</button>
-    </div>
+    </form>
   );
 };
 
 export default Form;
-
-// Install axios using: npm install axios
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const ContactForm = () => {
-//   const [formData, setFormData] = useState({
-//     name: '',
-//     email: '',
-//     message: ''
-//   });
-
-//   const handleChange = (e) => {
-//     const { name, value } = e.target;
-//     setFormData({
-//       ...formData,
-//       [name]: value
-//     });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     try {
-//       const response = await axios.post('http://127.0.0.1:3001/send-email', formData);
-//       alert(response.data);
-//     } catch (error) {
-//       alert('Error sending email:', error);
-//     }
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <label htmlFor="name">Name:</label>
-//       <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
-
-//       <label htmlFor="email">Email:</label>
-//       <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
-
-//       <label htmlFor="message">Message:</label>
-//       <textarea id="message" name="message" value={formData.message} onChange={handleChange} required></textarea>
-
-//       <button type="submit" className="btn">Submit</button>
-//     </form>
-//   );
-// };
-
-// export default ContactForm;
