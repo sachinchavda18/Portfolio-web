cb14c6085d30ad8c75a2c42d49885b6f1ff0860e
sachinchavda18 <120299726+sachinchavda18@users.noreply.github.com>
2023-11-26 12:25:41 +0530

    backend url

--- portfolio_frontend/src/utils/config.js
+++ portfolio_frontend/src/utils/config.js
@@ -1,3 +1,3 @@
 export const cloudinary_upload_preset = "Portfolio";
-export const backendUrl =  "http://localhost:8000";
+export const backendUrl = "https://portfolio-backend-6gjn.onrender.com" || "http://localhost:8000";
 // "https://portfolio-backend-6gjn.onrender.com" ||
