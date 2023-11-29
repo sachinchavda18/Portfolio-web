64d53b5834989f00217272e5e1ac86e4ccaa578c
sachinchavda18 <120299726+sachinchavda18@users.noreply.github.com>
2023-11-29 19:04:17 +0530

    Update config.js

--- portfolio_frontend/src/utils/config.js
+++ portfolio_frontend/src/utils/config.js
@@ -1,4 +1,4 @@
 export const cloudinary_upload_preset = "Portfolio";
 export const RESUME_URL = "https://drive.google.com/file/d/1cBDJRECooHqeExhzapitrhxmeEqxGQ6Z/view?usp=sharing"
-export const backendUrl = "http://localhost:8000";
+export const backendUrl = "https://portfolio-backend-6gjn.onrender.com" || "http://localhost:8000";
 // "https://portfolio-backend-6gjn.onrender.com" ||
